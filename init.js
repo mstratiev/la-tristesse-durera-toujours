91e20b7d8f1477ce65194a38cac8e1ab01f11df6
mstratiev <garmbrood@abv.bg>
2016-01-30 23:30:23 +0200

    init

---
+++ init.js
@@ -0,0 +1,51 @@
+! function() {
+
+var gogh = (function(){
+
+    var counts = {
+        a: 67,
+        d: 50,
+        e: 36,
+        j: 43,
+        i: 32,
+        l: 31,
+        o: 42,
+        r: 56,
+        s: 52,
+        t: 37,
+        u: 31
+    };
+
+    var randomN = function(N) {
+        return random = Math.floor(Math.random() * (N - 1 + 1)) + 1;
+    };
+    var im = function(letter) {
+        var r = randomN(counts[letter]) + '';
+        if (r.length == 1) {
+            r = '0' + r;
+        };
+        return letter+'/'+ letter + '_' + r + '.jpg';
+    };
+    var parse = function(word) {
+        var result = [],
+            len = word.length,
+            i;
+        for (i = 0; i < len; i += 1) {
+            result.push(im(word[i]));
+        }
+        return {letters: result}
+    }
+   	var data = {
+        la: parse('la'),
+        tristesse: parse('tristesse'),
+        durera: parse('durera'),
+        toujours: parse('toujours')
+    };
+    var template = '{{#each words}}<div class="word" id="{{@key}}">{{#each letters}}<img src="img/{{this}}"/>{{/each}}</div>{{/each}}'
+    var temp = Handlebars.compile(template);
+    $('#content').html(temp({"words":data}))
+
+
+
+})();
+}();
